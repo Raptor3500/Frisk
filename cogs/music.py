110d0fcfbe98a5b8a2da691bd9bc78da65ffc61d
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2019-01-31 16:16:25 -0500

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -7,3 +7,23 @@ class music():
         
     @commands.command(pass_context=True)
     async def play(self, ctx, *, url):
+        server = ctx.message.server
+        voice_client = self.bot.voice_client_in(server)
+        try:
+            if players[server.id].is_playing():
+                player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+
+            if server.id in queues:
+                queues[server.id].append(player)
+            else:
+                queues[server.id] = [player]
+                await bot.say('Video queued.')
+                else:
+                    player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+                    players[server.id] = player
+                    player.start()
+                    except KeyError:
+                        player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+                        players[server.id] = player
+                        player.start()
+                        
