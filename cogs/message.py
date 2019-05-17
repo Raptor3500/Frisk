245165f2bf02dfa9f89338ae4122e8efbfdd035f
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2019-05-16 20:42:23 -0400

    Update message.py

--- cogs/message.py
+++ cogs/message.py
@@ -10,8 +10,8 @@ class message():
         self.bot = bot
 
     async def on_message(self, message):
-        if 'want sum fucc?' in message.content:
-            await self.bot.send_message(message.channel, " HELL YEAH ")
+        if 'Fucc' in message.content:
+            await self.bot.send_message(message.channel, "I wanna fuck sun. FAITH HELP ME OUT HERE DO AN ERP WITH GIO")
             
         if not message.author.bot and (message.server == None or self.bot.user in message.mentions):
             await self.bot.send_typing(message.channel)
